d75ae17a45735f56e5006ecb32e5eaaf925cb84b
unknown <terencetan@hotmail.ca>
2021-07-28 15:22:37 -0400

    added some conditionals for when the user has guessed more than 6 times

--- src/components/Buttons/Buttons.jsx
+++ src/components/Buttons/Buttons.jsx
@@ -2,10 +2,8 @@ import React from "react";
 
 export default function Buttons(props) {
   return (
-    <div>
-      <button onClick={props.click} value={props.value}>
-        {props.letter}
-      </button>
-    </div>
+    <button onClick={props.click} value={props.value} disabled={props.disable}>
+      {props.letter}
+    </button>
   );
 }
--- src/components/Hangman/Hangman.jsx
+++ src/components/Hangman/Hangman.jsx
@@ -6,27 +6,42 @@ export default function Hangman() {
   let [guessed, setGuessed] = useState(new Set());
   let [answer, setAnswer] = useState("apple");
 
+  //showing the Guesses for example. If the word is money and [m,o,y] is guessed it will display as m o _ _ y
   let showGuess = () => {
     let split = answer.split("");
     let showGuess = split.map((inc) => (guessed.has(inc) ? inc : " _ "));
     return showGuess;
   };
 
+  //Adds guess to guessed letter state. Also adds number of wrong guesses if it is not
   let buttonClick = (e) => {
-    console.log(e.target.value);
+    setGuessed((prev) => new Set(prev).add(e.target.value));
+    setWGuesses(wGuesses + (answer.includes(e.target.value) ? 0 : 1));
   };
 
   let alphaButtons = "abcdefghijklmnopqrstuvwxyz"
     .split("")
     .map((ltr, idx) => (
-      <Buttons letter={ltr} key={idx} value={ltr} click={buttonClick} />
+      <Buttons
+        letter={ltr}
+        key={idx}
+        value={ltr}
+        click={buttonClick}
+        disable={guessed.has(ltr)}
+      />
     ));
+
+  let gameResults = (
+    <div>
+      <div>{showGuess()}</div>;{alphaButtons}
+    </div>
+  );
 
   return (
     <div>
-      <div>adsfdasf 3124</div>
-      <div>{showGuess()}</div>
-      {alphaButtons}
+      <div>Wrong Guesses:{wGuesses}/6</div>
+      <div>{guessed}</div>
+      {wGuesses >= 6 ? <div>You Lose LMAOOO</div> : gameResults}
     </div>
   );
 }
