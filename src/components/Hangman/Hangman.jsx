d15f088ed3e27b7931a19f4228204e912b5cd724
unknown <terencetan@hotmail.ca>
2021-07-28 16:09:55 -0400

    added documentation and using axios and useeffect to get words

--- src/components/Hangman/Hangman.jsx
+++ src/components/Hangman/Hangman.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Buttons from "../Buttons/Buttons";
+const axios = require("axios");
 
 export default function Hangman() {
   let [wGuesses, setWGuesses] = useState(0);
@@ -13,12 +14,20 @@ export default function Hangman() {
     return showGuess;
   };
 
+  useEffect(() => {
+    async function getData() {
+      let response = axios.get("https://random-words-api.vercel.app/word");
+      console.log(response);
+    }
+    return getData();
+  });
+
   //Adds guess to guessed letter state. Also adds number of wrong guesses if it is not
   let buttonClick = (e) => {
     setGuessed((prev) => new Set(prev).add(e.target.value));
     setWGuesses(wGuesses + (answer.includes(e.target.value) ? 0 : 1));
   };
-
+  //Creating the alphabete buttons
   let alphaButtons = "abcdefghijklmnopqrstuvwxyz"
     .split("")
     .map((ltr, idx) => (
@@ -31,9 +40,11 @@ export default function Hangman() {
       />
     ));
 
+  // setting a variable so that I can pass it through a terrinary operator
   let gameResults = (
     <div>
-      <div>{showGuess()}</div>;{alphaButtons}
+      <div>{showGuess()}</div>
+      {alphaButtons}
     </div>
   );
 
