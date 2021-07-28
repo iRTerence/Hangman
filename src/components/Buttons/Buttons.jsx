5596cef7796e844822d6a7123c5142a0b33c347a
unknown <terencetan@hotmail.ca>
2021-07-28 17:23:23 -0400

    added axios for random word api and styling

--- src/components/Buttons/Buttons.jsx
+++ src/components/Buttons/Buttons.jsx
@@ -1,8 +1,13 @@
 import React from "react";
+import "./Buttons.css";
 
 export default function Buttons(props) {
   return (
-    <button onClick={props.click} value={props.value} disabled={props.disable}>
+    <button
+      className='Buttons-btns'
+      onClick={props.click}
+      value={props.value}
+      disabled={props.disable}>
       {props.letter}
     </button>
   );
--- src/components/Hangman/Hangman.jsx
+++ src/components/Hangman/Hangman.jsx
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from "react";
 import Buttons from "../Buttons/Buttons";
+import "./Hangman.css";
 const axios = require("axios");
 
 export default function Hangman() {
   let [wGuesses, setWGuesses] = useState(0);
   let [guessed, setGuessed] = useState(new Set());
-  let [answer, setAnswer] = useState("apple");
+  let [answer, setAnswer] = useState("");
+  let [loaded, setLoaded] = useState(false);
+  let [hint, setHint] = useState("");
 
   //showing the Guesses for example. If the word is money and [m,o,y] is guessed it will display as m o _ _ y
   let showGuess = () => {
@@ -16,11 +19,15 @@ export default function Hangman() {
 
   useEffect(() => {
     async function getData() {
-      let response = axios.get("https://random-words-api.vercel.app/word");
-      console.log(response);
+      let response = await axios.get(
+        "https://random-words-api.vercel.app/word"
+      );
+      setAnswer(response.data[0].word);
+      setLoaded(true);
+      setHint(response.data[0].definition);
     }
     return getData();
-  });
+  }, [loaded]);
 
   //Adds guess to guessed letter state. Also adds number of wrong guesses if it is not
   let buttonClick = (e) => {
@@ -43,16 +50,37 @@ export default function Hangman() {
   // setting a variable so that I can pass it through a terrinary operator
   let gameResults = (
     <div>
-      <div>{showGuess()}</div>
+      <div className='Hangman-word'>{showGuess()}</div>
+      <div>{hint}</div>
       {alphaButtons}
     </div>
   );
-
-  return (
-    <div>
-      <div>Wrong Guesses:{wGuesses}/6</div>
-      <div>{guessed}</div>
-      {wGuesses >= 6 ? <div>You Lose LMAOOO</div> : gameResults}
+  // loading page
+  let loading = (
+    <div className='hangman-body'>
+      <div className='hangman-bg hangman-flex'>
+        <div className='hangman-c'></div>
+        <div className='hangman-line'></div>
+        <div className='hangman-r1'></div>
+        <div className='hangman-r2'></div>
+      </div>
     </div>
   );
+  // Set the losing display
+  let losing = (
+    <div>
+      <div>The answer is: {answer}</div>
+      <div>You Lose, lmao, what are you a LOSER?</div>
+    </div>
+  );
+
+  let results = (
+    <div className='Hangman'>
+      <h1>Hangman</h1>
+      <div>Wrong Guesses:{wGuesses}/6</div>
+      {wGuesses >= 6 ? losing : gameResults}
+    </div>
+  );
+
+  return <div>{loaded ? results : loading}</div>;
 }
